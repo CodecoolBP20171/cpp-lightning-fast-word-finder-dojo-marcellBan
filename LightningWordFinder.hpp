01b2a0a5964849540893d792dc8db00822670839
Bán Marcell <afga93+cc@gmail.com>
2017-11-22 10:02:32 +0100

    gotta go fast

--- LightningWordFinder.hpp
+++ LightningWordFinder.hpp
@@ -4,12 +4,30 @@
 
 #ifndef CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
 #define CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
-class LightningWordFinder
-{
+
+class LightningWordFinder {
 public:
-    std::string getLongestWord(const std::string& text)
-    {
-        return "Longest Word";
+    std::string getLongestWord(const std::string& text) {
+        size_t maxlen = 0ul;
+        size_t maxlen_start = 0ul;
+        size_t currlen = 0ul;
+        size_t currlen_start = 0ul;
+        for (int i = 0; i < text.length(); ++i) {
+            char c = text[i];
+            if ((c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z')) {
+                ++currlen;
+            } else {
+                if (currlen > maxlen) {
+                    maxlen = currlen;
+                    maxlen_start = currlen_start;
+                }
+                currlen = 0ul;
+                currlen_start = i + 1ul;
+            }
+        }
+        return text.substr(maxlen_start, maxlen);
     }
 };
+
 #endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
